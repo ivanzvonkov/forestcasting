7cb1010b4911eda192aed76a5154680389046391
shimakanani <Shima@Shimas-MacBook-Pro-3.local>
2019-11-05 18:42:55 -0500

    added comments, switched to pandas, fixed csv routes

---
+++ Scripts/normalize_data.py
@@ -0,0 +1,77 @@
+"""
+Inputs: (1) shape file components of province vegetation index
+            (a) AVICrownIndex.shp
+            (b) AVICrownIndex.prj
+            (c) AVICrownIndex.shx
+        (2) {prov}_location_grid.csv : csv of province location key grid to be narrowed
+Output: {prov}_location_grid_usefulness.csv
+"""
+
+import pyproj
+from functools import partial
+from shapely.geometry import box
+from shapely.ops import transform
+from shapely.geometry import Point
+import fiona
+import pandas as pd
+from pathlib import Path
+
+
+def read_shape_file():
+    """
+    Reads shape file from AVICrownIndex.shp and converts coordinates to latitude and longitude format
+    :return: shapely Polygon of converted forest coordinates within the province
+    """
+    # import shape file
+    shape = fiona.open("../Data/Location/AVICrownIndex.shp")
+
+    # convert shape file into a Polygon of lat/long coordinates
+    p_in = pyproj.Proj(shape.crs)
+    bound_box = box(*shape.bounds)
+    p_out = pyproj.Proj({'init': 'EPSG:4326'})  # aka WGS84
+    project = partial(pyproj.transform, p_in, p_out)
+    bound_box_wgs84 = transform(project, bound_box)
+
+    # return converted polygon
+    return bound_box_wgs84
+
+
+def update_location_grid(input_polygon, location_df):
+    """
+    Reads location grid keys and eliminates coordinates outside of input_polygon
+    :param input_polygon: Polygon of forest coordinates in province
+    :param location_df: Pandas data frame with complete locations keys of province
+    :return: Pandas data frame with location keys that fall within input_polygon
+    """
+
+    # create updated list to add the rows inside the Polygon
+    valid_points = []
+
+    # keep track of the number of points inside / outside of the polygon
+    inside = 0
+    outside = 0
+
+    for index, row in location_df.iterrows():
+        if index > 0:
+            # turn every coordinate into a shapely Point
+            lat = row[1]
+            lon = row[2]
+            point = Point(float(lon), float(lat))
+
+            # check if the polygon contains the point
+            if input_polygon.contains(point):
+                inside += 1
+                valid_points.append(row)
+            else:
+                outside += 1
+
+    # return updated location grid
+    return pd.DataFrame(valid_points, columns=['KEY', 'LATITUDE', 'LONGITUDE'])
+
+
+if __name__ == "__main__":
+    forest_polygon = read_shape_file()
+    df = pd.read_csv(Path('../Data/Location/alberta_grid_system.csv'))
+    updated_df = update_location_grid(forest_polygon, df)
+    updated_df.to_csv('../Data/Location/AL_location_grid_usefulness.csv')
+

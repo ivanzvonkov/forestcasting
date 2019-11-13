8afbec9499289ea62eb0dc4f98a90336b299da8a
shimakanani <Shima@Shimas-MacBook-Pro-3.local>
2019-11-13 18:01:11 -0500

    added new key

--- Scripts/join_datasets.py
+++ Scripts/join_datasets.py
@@ -2,9 +2,9 @@
 Inputs: (1) Weather location data: {prov}_weather_location_grid.csv
         (2) Fire location data: {prov}_fire_location_grid.csv
         (2) Location grid: {prov}_location_grid.csv
-Output: {prov}__dataset.csv
+Output: {prov}_dataset.csv
         & prints stats about null columns
-Command: python3.6 join_dataset {prov}
+Command: python3.6 join_datasets.py {prov}
 """
 
 import pandas as pd
@@ -18,21 +18,26 @@ if __name__ == "__main__":
     province = input[:1][0]
     filename = f'{province}_dataset.csv'
 
-    # read fire data
+    # read fire data and create new key
     fire_df = pd.read_csv(Path(f'../Data/Fire/{province}_fire_location_grid.csv'))
+    fire_df["KEY"] = fire_df["locationKey"].map(str) + "|" + fire_df["START_DATE"]
 
-    # read weather data and delete unused columns
+    # read weather data
     weather_df = pd.read_csv(Path('../Data/Weather/AL_weather_location_grid.csv'), low_memory=False)
-    cols_to_delete = ['KEY', 'STATION ID', 'Station Name', 'Longitude (x)', 'Latitude (y)', 'Climate ID', 'Date/Time',
+    cols_to_delete = ['KEY', 'STATION ID', 'Station Name', 'Longitude (x)', 'Latitude (y)', 'Climate ID',
                       'Year', 'Month', 'Day', 'Data Quality', 'Max Temp Flag', 'Min Temp Flag', 'Mean Temp Flag',
                       'Heat Deg Days Flag', 'Cool Deg Days Flag', 'Total Rain Flag', 'Total Snow Flag',
-                      'Total Precip Flag',
-                      'Snow on Grnd Flag', 'Dir of Max Gust Flag', 'Spd of Max Gust Flag', ]
+                      'Total Precip Flag', 'Snow on Grnd Flag', 'Dir of Max Gust Flag', 'Spd of Max Gust Flag', ]
+
+    # delete unused columns
     for i in cols_to_delete:
         del weather_df[i]
 
-    # one to one merge to combine fire and weather data
-    merged_df = pd.merge(fire_df, weather_df, left_on='locationKey', right_on='LOCATION KEY')
+    # create new key
+    weather_df["KEY"] = weather_df["LOCATION KEY"].map(str) + "|" + weather_df["Date/Time"]
+
+    # left merge to combine fire and weather data
+    merged_df = pd.merge(fire_df, weather_df, how='left')
 
     # read location data
     location_df = pd.read_csv(Path('../Data/Location/alberta_grid_system.csv'))

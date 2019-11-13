d0813607a30fc25156d2561276a805e720e8b4f1
shimakanani <Shima@Shimas-MacBook-Pro-3.local>
2019-11-12 21:41:53 -0500

    added location grid

--- Scripts/join_datasets.py
+++ Scripts/join_datasets.py
@@ -1,31 +1,44 @@
 """
-Inputs: (1) Weather location grid CSV : {prov}_weather_location_grid.csv
-        (2) Fire location grid CSV : {prov}_fire_location_grid.csv
-        (2) Narrowed location key CSV: {prov}_location_grid.csv
+Inputs: (1) Weather location data: {prov}_weather_location_grid.csv
+        (2) Fire location data: {prov}_fire_location_grid.csv
+        (2) Location grid: {prov}_location_grid.csv
 Output: {prov}__dataset.csv
-Command: python join_dataset {prov}
+        & prints stats about null columns
+Command: python3.6 join_dataset {prov}
 """
 
 import pandas as pd
 from pathlib import Path
 import sys
 
+
 if __name__ == "__main__":
     # set file name (province) from command line
-    province = sys.argv[1:]
-    filename = f'{province[:1][0]}__dataset.csv'
+    input = sys.argv[1:]
+    province = input[:1][0]
+    filename = f'{province}_dataset.csv'
 
     # read fire data
-    fire_df = pd.read_csv(Path('../Data/Fire/AL_fire_location_grid.csv'))
+    fire_df = pd.read_csv(Path(f'../Data/Fire/{province}_fire_location_grid.csv'))
 
     # read weather data and delete unused columns
     weather_df = pd.read_csv(Path('../Data/Weather/AL_weather_location_grid.csv'), low_memory=False)
-    cols_to_delete = ['KEY', 'STATION ID', 'Station Name', 'Longitude (x)', 'Latitude (y)', 'Climate ID', 'Date/Time', 'Year', 'Month', 'Day', 'Data Quality', 'Max Temp Flag', 'Min Temp Flag', 'Mean Temp Flag', 'Heat Deg Days Flag', 'Cool Deg Days Flag', 'Total Rain Flag', 'Total Snow Flag', 'Total Precip Flag', 'Snow on Grnd Flag', 'Dir of Max Gust Flag', 'Spd of Max Gust Flag',]
+    cols_to_delete = ['KEY', 'STATION ID', 'Station Name', 'Longitude (x)', 'Latitude (y)', 'Climate ID', 'Date/Time',
+                      'Year', 'Month', 'Day', 'Data Quality', 'Max Temp Flag', 'Min Temp Flag', 'Mean Temp Flag',
+                      'Heat Deg Days Flag', 'Cool Deg Days Flag', 'Total Rain Flag', 'Total Snow Flag',
+                      'Total Precip Flag',
+                      'Snow on Grnd Flag', 'Dir of Max Gust Flag', 'Spd of Max Gust Flag', ]
     for i in cols_to_delete:
         del weather_df[i]
 
-    # outer merge
-    merged_df = pd.merge(left=fire_df, right=weather_df, how='outer', left_on='locationKey', right_on='LOCATION KEY')
+    # one to one merge to combine fire and weather data
+    merged_df = pd.merge(fire_df, weather_df, left_on='locationKey', right_on='LOCATION KEY')
+
+    # read location data
+    location_df = pd.read_csv(Path('../Data/Location/alberta_grid_system.csv'))
+
+    # outer merge with location grid
+    merged_df = pd.merge(merged_df, location_df, how='outer', left_on='locationKey', right_on='KEY')
 
     # print null columns count
     null_columns = merged_df.columns[merged_df.isnull().any()]
@@ -34,3 +47,4 @@ if __name__ == "__main__":
     # output csv
     merged_df.to_csv(Path(f'../Data/Location/{filename}'))
     print('Wrote to ' + str(Path(f'../Data/Location/{filename}')))
+

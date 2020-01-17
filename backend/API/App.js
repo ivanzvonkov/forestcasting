7eb91866b3e391816479af310ca0acc601f37c4f
Sam Campbell <scamp94@uwo.ca>
2020-01-17 10:28:32 -0500

    add route for analysis

--- backend/API/App.js
+++ backend/API/App.js
@@ -1,6 +1,9 @@
 const express = require("express");
-const bodyParser = require('body-parser');
-const querystring = require('querystring');
+const bodyParser = require("body-parser");
+const querystring = require("querystring");
+const EcoData = require("../Schemas/EcoData.js")
+const HistoricData = require("../Schemas/HistoricData.js")
+const WeatherData = require("../Schemas/WeatherData.js")
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -10,6 +13,9 @@ app.get("/getAnalysis", async function (req, res, next) => {
   let lat = req.query.lat
   let lng = req.query.lng
   let date = req.query.date
+
+  let locationKey = lat + "|" + lng
+
 
 })
 
--- backend/API/Server.js
+++ backend/API/Server.js
@@ -5,6 +5,6 @@ const port = process.env.PORT || 3100;
 const server = http.createServer(app);
 
 server.listen(port, () => {
-    //    let's print a message when the server run successfully
-    console.log("Server restarted successfully")
+    // print a message when the server run successfully
+    console.log("Server started successfully")
 });

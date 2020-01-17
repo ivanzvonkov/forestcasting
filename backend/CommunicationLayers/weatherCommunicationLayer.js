ae8055478f1adf72ef47984b9d4aca7ab531b933
Sam Campbell <scamp94@uwo.ca>
2020-01-17 13:19:34 -0500

    updated weather to use new api that gives forecast

--- backend/CommunicationLayers/weatherCommunicationLayer.js
+++ backend/CommunicationLayers/weatherCommunicationLayer.js
@@ -4,8 +4,8 @@ const dotenv = require('dotenv')
 dotenv.config()
 const api_key = process.env.API_KEY
 
-function get_weather(lat, lon){
-    url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + api_key
+function get_weather(lat, lng){
+    url = 'https://api.weatherbit.io/v2.0/forecast/daily?key=' + api_key + '&lat=' + lat + '&lon=' + lng
     return new Promise(function(resolve, reject) {
       requests(url, {json : true}, (err, res, body) => {
       resolve(body)
@@ -13,43 +13,27 @@ function get_weather(lat, lon){
   })
 }
 
-async function weather_data(lat, lon){
-    let weather = await get_weather(lat, lon)
+async function weather_data(lat, lng){
+    let weather = await get_weather(lat, lng)
+    weather = weather["data"]
 
-    if (weather['cod'] == 200) {
-        // Set precipitation default values
-        let TOTAL_RAIN = 0.0
-        let TOTAL_SNOW = 0.0
-        // Temperatures are return in kelvin - convert to celsuis
-        let MAX_TEMP = weather['main']['temp_max'] - 273.15
-        let MIN_TEMP = weather['main']['temp_min'] - 273.15
-        let MEAN_TEMP = (MIN_TEMP) + MAX_TEMP/ 2
-
-        if ('rain' in weather){
-            TOTAL_RAIN = weather['rain']['1h']  // Rain for last 1hour in mm
-          }
-
-        if ('snow' in weather){
-            TOTAL_SNOW = weather['snow']['1h']  // Snow for last 1hour in mm
-          }
-
-        let TOTAL_PRECIP = TOTAL_RAIN + TOTAL_SNOW
-
-        // Wind default values
-        let DIR_OF_MAX_GUST = 0.0
-        let SPD_OF_MAX_GUST = 0.0
-        if ('wind' in weather) {
-            DIR_OF_MAX_GUST = weather['wind']['deg']  // Wind direction in deg
-            SPD_OF_MAX_GUST = weather['wind']['speed'] // Wind speed in m/s
-          }
-
-        let current_weather = new WeatherData(MAX_TEMP, MIN_TEMP,
-                                      MEAN_TEMP, TOTAL_RAIN, TOTAL_SNOW, TOTAL_PRECIP, DIR_OF_MAX_GUST, SPD_OF_MAX_GUST)
-
-        return current_weather
-    }
-    else {
-        console.log('API call did not work')
-        return null  // API call was invalid
-      }
+    let results = []
+    weather.forEach(element => {
+      results.push( new WeatherData(
+          element["valid_date"],
+          element["max_temp"],
+          element["min_temp"],
+          element["temp"],
+          element["precip"],
+          element["snow"],
+          element["snow_depth"],
+          element["wind_spd"],
+          element["wind_gust_spd"],
+          element["wind_dir"]
+        ))
+    })
+    console.log(results)
+    return results
   }
+
+weather_data(49.2, -113.6)

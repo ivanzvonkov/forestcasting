78db716ebf526faccb094fb105b72a5caa8a1df9
SHIMA KANANITODASHKI <skananit@uwo.ca>
2020-01-17 13:24:20 -0500

    added pin

--- frontend/src/components/MapPage/Map/Map.js
+++ frontend/src/components/MapPage/Map/Map.js
@@ -1,23 +1,34 @@
 import React, { useState } from "react";
 import GoogleMap from "google-map-react";
 import axios from "axios";
+import "./style.css";
 
 export const Map = () => {
+  const [selectedLat, setSelectedLat] = useState(null);
+
+  const [selectedLng, setSelectedLng] = useState(null);
+
+  const [defaultCenter] = useState({
+    lat: 56.41630331167829,
+    lng: -114.81301656871508
+  });
+
   const [selectedLocation, setSelectedLocation] = useState(
     "Click to select a location."
   );
 
-  const [defaultCenter] = useState({ lat: 62.227, lng: -105.3809 });
-
   const onClick = event => {
-    console.log("Lat:", event.lat);
-    console.log("Lng:", event.lng);
+    setSelectedLat(event.lat);
+    setSelectedLng(event.lng);
+
     axios
       .get(
         `https://maps.googleapis.com/maps/api/geocode/json?latlng=${event.lat},${event.lng}&key=${process.env.REACT_APP_GEOCODING_API_KEY}`
       )
       .then(res => {
-        setSelectedLocation(res.data.results[0].formatted_address);
+        setSelectedLocation(
+          "Selected: " + res.data.results[0].formatted_address
+        );
       });
   };
 
@@ -28,10 +39,21 @@ export const Map = () => {
         bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
         defaultCenter={defaultCenter}
         center={defaultCenter}
-        defaultZoom={3}
+        defaultZoom={6}
         onClick={onClick}
-      />
+      >
+        {selectedLat ? <Marker lat={selectedLat} lng={selectedLng} /> : null}
+      </GoogleMap>
     </div>
   );
 };
 export default Map;
+
+const Marker = () => {
+  return (
+    <>
+      <div className="pin"></div>
+      <div className="pulse"></div>
+    </>
+  );
+};

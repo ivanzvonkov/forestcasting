38475cc79718d8ef291bd3bac0317d778778fc2c
Sam Campbell <scamp94@uwo.ca>
2020-01-21 08:34:36 -0500

    hard coded eco stats until they are added to db

--- backend/Schemas/EcoData.js
+++ backend/Schemas/EcoData.js
@@ -3,9 +3,12 @@ class EcoData {
       this.KEY = KEY
       this.LATITUDE = LAT
       this.LONGITUDE = LON
-      this.ECOZONE = ECOZONE
-      this.ECOREGION = ECOREGION
-      this.ECODISTRICT = ECODISTRICT
+      this.ZONE = ECOZONE
+      this.REGION = ECOREGION
+      this.DISTRICT = ECODISTRICT
+      this.DESCRIPTION = "The Prairie Ecozone occupies a semi-circular area that has its base on the Canada-U.S. border and arcs from the western edge of Alberta to the eastern edge of Manitoba. Climatic characteristics include over 400 mm of precipitation annually. The mean daily January temperatures of -22.5ºC to -25ºC and mean daily July temperatures of 15ºC to 17.5ºC."
+      this.AVERAGEFIREDURATIONFORZONE = 44
+      this.AVERAGEFIRESIZEFORZONE = 29
     }
   }
 

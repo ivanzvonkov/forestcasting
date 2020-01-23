ec6e36154ea616443a466e05745cb15755c53237
ivanzvonkov <ivan.zvonkov@gmail.com>
2020-01-22 22:15:03 -0500

    No need to location in eco

--- backend/Schemas/EcoData.js
+++ backend/Schemas/EcoData.js
@@ -1,8 +1,6 @@
 class EcoData {
-    constructor (KEY, LAT, LON, ECOZONE, ECOREGION, ECODISTRICT) {
+    constructor (KEY, ECOZONE, ECOREGION, ECODISTRICT) {
       this.locationKey = KEY
-      this.latitude = LAT
-      this.longitude = LON
       this.zone = ECOZONE
       this.region = ECOREGION
       this.district = ECODISTRICT
